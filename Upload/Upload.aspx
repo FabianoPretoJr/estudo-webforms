45ff37b60011f5c54b8a8d7535e88c0881b9b16b
foeu <fabiano.junior@gft.com>
2021-06-10 11:54:44 -0300

    Implementado upload de arquivos

---
+++ Upload/Upload.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Upload.aspx.cs" Inherits="Upload.Upload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="UPLOAD de Arquivos"></asp:Label>
+            <br />
+            <br />
+            <asp:FileUpload ID="fuArquivo" runat="server" AllowMultiple="True" />
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Nome do arquivo: "></asp:Label>
+            <asp:TextBox ID="txtNome" runat="server" Width="862px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Tamanho do arquivo: "></asp:Label>
+            <asp:TextBox ID="txtTamanho" runat="server" Width="840px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btnEnviar" runat="server" OnClick="btnEnviar_Click" Text="Enviar Arquivo" Width="182px" />
+            <asp:Button ID="btnEnviar2" runat="server" OnClick="btnEnviar2_Click" Text="Enviar Multiplos Arquivos" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Upload/Upload.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Upload
+{
+    public partial class Upload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnEnviar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //em casos reais é bom controlar o tamanho de arquivos e usar try..catch
+                string nomeArquivo = fuArquivo.FileName;
+                txtNome.Text = nomeArquivo;
+                txtTamanho.Text = fuArquivo.PostedFile.ContentLength.ToString() + "Kb";
+
+                string path = Server.MapPath(@"Upload\"); //não esquece a barra pra funcionar a concatenação e pra ele entender que isso é uma pasta
+                fuArquivo.PostedFile.SaveAs(path + nomeArquivo);
+            }
+            catch
+            {
+
+            }
+        }
+
+        protected void btnEnviar2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //em casos reais é bom controlar o tamanho de arquivos e usar try..catch
+                string nomeArquivos = "";
+                string path = Server.MapPath(@"Upload\"); //não esquece a barra pra funcionar a concatenação e pra ele entender que isso é uma pasta
+
+                foreach (var arquivo in fuArquivo.PostedFiles)
+                {
+                    string nomeArquivo = arquivo.FileName;
+                    arquivo.SaveAs(path + nomeArquivo);
+                    nomeArquivos = nomeArquivos + nomeArquivo + " - ";
+                }
+
+                txtNome.Text = nomeArquivos;
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
